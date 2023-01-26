d38fb4b96ac5359cee81a85f7f407c357e91d154
Julia Kulev <75759047+Zhiiuu@users.noreply.github.com>
2023-01-26 12:45:24 -0800

    Add files via upload

--- walk.js
+++ walk.js
@@ -1,11 +1,11 @@
 class walk {
     constructor(game) {
         this.game = game;
-        this.animator = new Animator(ASSET_MANAGER.getAsset("./walk.png"), 0, 0, 160, 160, 10, 0.2);
+        //this.animator = new Animator(ASSET_MANAGER.getAsset("./walk.png"), 0, 0, 160, 160, 10, 0.2);
 
         this.facing = 0;
 
-        this.x = -100;
+        this.x = 0;
         this.y = 615;
         this.speed = 0;
 
@@ -20,31 +20,38 @@ class walk {
         }
 
         this.animations[0] = new Animator(ASSET_MANAGER.getAsset("./walk.png"), 0, 0, 160, 160, 10, 0.2);
-        this.animations[1] = new Animator(ASSET_MANAGER.getAsset("./walk.png"), 0, 0, 160, 160, 10, 0.2);
-        this.animations[2] = new Animator(ASSET_MANAGER.getAsset("./walk.png"), 0, 0, 160, 160, 10, 0.2);
+        this.animations[1] = new Animator(ASSET_MANAGER.getAsset("./walkleft.png"), 0, 0, 160, 160, 10, 0.2);
+        this.animations[2] = new Animator(ASSET_MANAGER.getAsset("./walkleft.png"), 0, 0, 160, 160, 10, 0.2);
         this.animations[3] = new Animator(ASSET_MANAGER.getAsset("./walk.png"), 0, 0, 160, 160, 10, 0.2)
+        this.animations[4] = new Animator(ASSET_MANAGER.getAsset("./idle1.png"), 0, 0, 160, 160, 4, 0.2)
 
     };
 
     update() {
         this.x += this.speed*this.game.clockTick;
 
-        if (game.keys['w'] && !game.keys['s'] && !game.keys[' ']) {
-            this.facing = 3;
+        if (game.keys['w']) {
+            this.facing = 0;
             this.y -= 2;
         }
-        else if (game.keys['s'] && !game.keys['w'] && !game.keys[' ']) {
-            this.facing = 0;
+        else if (game.keys['s']) {
+            this.facing = 1;
             this.y += 2;
         }
         //determine horizontal
-        if (game.keys['a'] && !game.keys['d'] && !game.keys[' ']) {
-            this.facing = 1;
+        if (game.keys['a']) {
+            this.facing = 2;
             this.x -= 2;
         }
-        else if (game.keys['d'] && !game.keys['a'] && !game.keys[' ']) {
-            this.facing = 2;
+        else if (game.keys['d']) {
+            this.facing = 3;
             this.x += 2;
+        }
+
+        if (!game.keys['w'] && !game.keys['s'] && !game.keys['d'] && !game.keys['a']) {
+            this.facing = 4;
+            this.x -= 0;
+            this.y -= 0;
         }
 
         if(this.x > 1024) this.x = -100;
@@ -54,4 +61,5 @@ class walk {
     draw(ctx) {
         this.animations[this.facing].drawFrame(this.game.clockTick, ctx, this.x, this.y);
     };
+
 }
